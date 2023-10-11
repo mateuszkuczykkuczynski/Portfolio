24ba2f4c67c636c613561f2cbd68706910e3520e
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-10-11 23:53:21 +0200

    feat/tailwind_setup_added

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AboutMe from './componsents/AboutMe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
